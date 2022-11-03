4299c30de732467f6a8693541e5d0a4e10a4692f
jack battle <jackonno78@hotmail.com>
2022-11-03 17:37:23 -0500

    main ui tuned up

--- DECRYPTo/DECRYPTo/Controllers/ViewController.swift
+++ DECRYPTo/DECRYPTo/Controllers/ViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var decryptoLabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
     }
 
 
