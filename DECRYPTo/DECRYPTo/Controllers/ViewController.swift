72593169380f317c90d858cd1ac36c3202fa992b
jack battle <jackonno78@hotmail.com>
2022-11-03 13:58:34 -0500

    app restructure, min version raised to 13

--- DECRYPTo/DECRYPTo/Controllers/ViewController.swift
+++ DECRYPTo/DECRYPTo/Controllers/ViewController.swift
@@ -9,12 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let wordlist = Wordlist()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Total dict count: \(wordlist.wordlistDict.count)")
+        
     }
 
 
--- DECRYPTo/DECRYPTo/Controllers/WordListViewController.swift
+++ DECRYPTo/DECRYPTo/Controllers/WordListViewController.swift
@@ -12,28 +12,13 @@ class WordListViewController: ViewController, UITableViewDelegate, UITableViewDa
     @IBOutlet weak var WordListTableView: UITableView!
     
     var list = Wordlist()
-    
-    var keys = [Int]()
-    var values = [String]()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         WordListTableView.delegate = self
         WordListTableView.dataSource = self
+        WordListTableView.allowsSelection = false
     }
-    
-    func generateKeys() {
-        for key in list.wordlistDict {
-            keys.append(key.key)
-        }
-    }
-    func generateValues() {
-        for value in list.wordlistDict {
-            values.append(value.value)
-        }
-    }
-    
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.wordlistDict.count
@@ -48,7 +33,4 @@ class WordListViewController: ViewController, UITableViewDelegate, UITableViewDa
 
         return cell
     }
-    
-
-    
 }
