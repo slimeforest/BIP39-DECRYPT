2ea01dd59adb2036a5106655b8dbab3d62723f25
jack battle <jackonno78@hotmail.com>
2022-11-03 18:14:39 -0500

    can now copy cc addresses

--- DECRYPTo/DECRYPTo/Controllers/ViewController.swift
+++ DECRYPTo/DECRYPTo/Controllers/ViewController.swift
@@ -8,14 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // outlets
     @IBOutlet weak var decryptoLabelOutlet: UILabel!
+    @IBOutlet weak var btcAddressOutlet: UILabel!
+    @IBOutlet weak var ethAddressOutlet: UILabel!
+    @IBOutlet weak var adaAddressOutlet: UILabel!
+    @IBOutlet weak var dogeAddressOutlet: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
     }
+    
+    // address buttons pressed
+    @IBAction func btcButtonPressed(_ sender: Any) {
+        UIPasteboard.general.string = btcAddressOutlet.text
+        presentAlert(btnPressed: "BTC")
+    }
+    @IBAction func ethButtonPressed(_ sender: Any) {
+        UIPasteboard.general.string = ethAddressOutlet.text
+        presentAlert(btnPressed: "ETH")
+    }
+    @IBAction func adaButtonPressed(_ sender: Any) {
+        UIPasteboard.general.string = adaAddressOutlet.text
+        presentAlert(btnPressed: "ADA")
+    }
+    @IBAction func dogeButtonPressed(_ sender: Any) {
+        UIPasteboard.general.string = dogeAddressOutlet.text
+        presentAlert(btnPressed: "DOGE")
+    }
+    
+    // alert
+    func presentAlert(btnPressed: String) {
+        let alert = UIAlertController(title: "Copied!", message: "\(btnPressed) address has been copied to the clipboard.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
 
-
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
