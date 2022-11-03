ec8d19156f9a0b8e5cbc97d18f2e67ced44b5009
jack battle <jackonno78@hotmail.com>
2022-11-03 16:40:17 -0500

    reworked home screen

--- DECRYPTo/DECRYPTo/Controllers/DecryptViewController.swift
+++ DECRYPTo/DECRYPTo/Controllers/DecryptViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class DecryptViewController: ViewController, UITextFieldDelegate {
     
+    
+    // outlets
     @IBOutlet weak var numberEntryField: UITextField!
     @IBOutlet weak var mainWordLabelOutlet: UILabel!
     @IBOutlet weak var fullStringLabelOutlet: UILabel!
+    @IBOutlet weak var copyButtonOutlet: UIButton!
+    @IBOutlet weak var deleteButtonOutlet: UIButton!
     
+    let list = Wordlist()
+    
+    override func viewDidLoad() {
+        print("Decrypt view controller successfully loaded")
+        numberEntryField.delegate = self
+        
+        deleteButtonOutlet.tintColor = .red
+    }
+    
+    //button actions
     @IBAction func copyButtonPressed(_ sender: Any) {
         print("copy button pressed")
     }
@@ -21,11 +35,8 @@ class DecryptViewController: ViewController, UITextFieldDelegate {
         print("delete button pressed")
     }
     
-    override func viewDidLoad() {
-        print("Decrypt view controller successfully loaded")
-        numberEntryField.delegate = self
-    }
     
+    // textfield character limit
     let characterlimit = 4
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -33,18 +44,30 @@ class DecryptViewController: ViewController, UITextFieldDelegate {
         return (textField.text?.utf16.count ?? 0) + string.utf16.count - range.length <= characterlimit
     }
     
+    // textfield updated
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        print("func called and current text: \(textField.text!)")
         checkCharacterCount(textField)
     }
     
     func checkCharacterCount(_ field: UITextField) {
         if field.text?.count == 4 {
-            print("current text: \(field.text!)")
-            mainWordLabelOutlet.text = field.text
+           let foundWord = checkWord(field.text!)
+    
+            mainWordLabelOutlet.text = foundWord
         }else {
             print("current text else case: \(field.text!)")
         }
     }
     
+    func checkWord(_ number: String) -> String {
+        let fourDigitNumber = Int(number)
+        print("Int: \(fourDigitNumber ?? 9999)")
+        
+        if let word = list.wordlistDict[fourDigitNumber!] {
+            print(word)
+            return word
+        }else {
+            return "Not Found"
+        }
+    }
 }
