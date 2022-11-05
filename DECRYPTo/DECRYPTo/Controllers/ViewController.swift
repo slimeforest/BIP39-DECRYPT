ddea5543e05472b38fd13440891945e992db6692
jack battle <jackonno78@hotmail.com>
2022-11-05 15:16:15 -0500

    cleaned up the main menu and added encrypt button

--- DECRYPTo/DECRYPTo/Controllers/ViewController.swift
+++ DECRYPTo/DECRYPTo/Controllers/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
     
     // outlets
     @IBOutlet weak var decryptoLabelOutlet: UILabel!
@@ -16,28 +16,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var adaAddressOutlet: UILabel!
     @IBOutlet weak var dogeAddressOutlet: UILabel!
     
+    let btcAddress = "bc1pmm7htgzphh8e2n8j8ghrqhg5j66mc6z6kqupp7e5xnlpu8zp0v4qyhwn8w"
+    let ethAddress = "0xe6FDE716da7853cCa69696A4c9264AE0C0ac1574"
+    let adaAddress = "addr1qxceekr0c2vnv3esdzzxaee4xk9fv2katnuclhfdz87sl84rkn6apwfxe2whv3wasec3hcqyf433h2t2ms7hves3gppqr4dzqc"
+    let dogeAddress = "DJYhUxw4isHWuCQU3fKkgWwuSz2qfeBC8d"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        configMenu()
     }
     
-    // address buttons pressed
-    @IBAction func btcButtonPressed(_ sender: Any) {
-        UIPasteboard.general.string = btcAddressOutlet.text
-        presentAlert(btnPressed: "BTC")
-    }
-    @IBAction func ethButtonPressed(_ sender: Any) {
-        UIPasteboard.general.string = ethAddressOutlet.text
-        presentAlert(btnPressed: "ETH")
-    }
-    @IBAction func adaButtonPressed(_ sender: Any) {
-        UIPasteboard.general.string = adaAddressOutlet.text
-        presentAlert(btnPressed: "ADA")
-    }
-    @IBAction func dogeButtonPressed(_ sender: Any) {
-        UIPasteboard.general.string = dogeAddressOutlet.text
-        presentAlert(btnPressed: "DOGE")
+    @IBOutlet weak var addressButtonOutlet: UIBarButtonItem!
+    
+    
+    func configMenu() {
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Copy BTC address", image: UIImage(systemName: "sun.max"), handler: { (_) in
+                    print("BTC Pressed.")
+                    UIPasteboard.general.string = self.btcAddress
+                    self.presentAlert(btnPressed: "BTC")
+                }),
+                UIAction(title: "Copy ETH address", image: UIImage(systemName: "sun.max"), handler: { (_) in
+                    print("ETH Pressed.")
+                    UIPasteboard.general.string = self.ethAddress
+                    self.presentAlert(btnPressed: "ETH")
+                }),
+                UIAction(title: "Copy ADA address", image: UIImage(systemName: "sun.max"), handler: { (_) in
+                    print("ADA Pressed.")
+                    UIPasteboard.general.string = self.adaAddress
+                    self.presentAlert(btnPressed: "ADA")
+                }),
+                UIAction(title: "Copy DOGE address", image: UIImage(systemName: "sun.max"), handler: { (_) in
+                    print("DOGE Pressed.")
+                    UIPasteboard.general.string = self.dogeAddress
+                    self.presentAlert(btnPressed: "DOGE")
+                })
+            ]
+        }
+
+        var demoMenu: UIMenu {
+            return UIMenu(title: "Enjoying?", image: nil, identifier: nil, options: [], children: menuItems)
+        }
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Menu", image: nil, primaryAction: nil, menu: demoMenu)
+    
     }
     
     // alert
